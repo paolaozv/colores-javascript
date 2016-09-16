8d65e28655cb28e61354f2d13bf6acf63fd096f9
Paola Ortiz de Zevallos <paolaozv@gmail.com>
2016-09-15 19:08:25 -0500

    subiendo cambio de colores

---
+++ js/main.js
@@ -0,0 +1,44 @@
+window.addEventListener("load", function() {
+	var boton = document.getElementById("cri");
+	var contador = 1;
+	boton.addEventListener("click", function() {
+		if(contador == 1) {
+			document.getElementById("color").value = "Morado";
+			document.getElementById("purple").classList.add("border-purple");
+			document.getElementById("green").classList.remove("border-green");
+		}
+		if(contador == 2) {
+			document.getElementById("color").value = "Amarillo";
+			document.getElementById("yellow").classList.add("border-yellow");
+			document.getElementById("purple").classList.remove("border-purple");
+		}
+		if(contador == 3) {
+			document.getElementById("color").value = "Verde";
+			document.getElementById("green").classList.add("border-green");
+			document.getElementById("yellow").classList.remove("border-yellow");
+			contador = 0;
+		}
+		contador++;
+	});
+	
+});
+
+window.addEventListener("load", function() {
+	var boton = document.getElementById("superCri");
+	boton.addEventListener("click", function() {
+		var color = document.getElementById("color").value;
+		if (color == "morado") {
+			document.getElementById("purple").classList.toggle("border-purple");
+			document.getElementById("yellow").classList.remove("border-yellow");
+			document.getElementById("green").classList.remove("border-green");
+		} else if (color == "amarillo") {
+			document.getElementById("yellow").classList.toggle("border-yellow");
+			document.getElementById("purple").classList.remove("border-purple");
+			document.getElementById("green").classList.remove("border-green");
+		} else if (color == "verde") {
+			document.getElementById("green").classList.toggle("border-green");
+			document.getElementById("yellow").classList.remove("border-yellow");
+			document.getElementById("purple").classList.remove("border-purple");
+		}
+	});
+});
